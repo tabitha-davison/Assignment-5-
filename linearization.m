82a461d901e99bf39a28e021517c5fdec73755d4
Vaani Bhatnagar <98102364+vaanibhatnagar@users.noreply.github.com>
2024-11-18 20:58:20 -0500

    box params updated linearization

--- linearization.m
+++ linearization.m
@@ -1,30 +1,22 @@
 clear;
 % System parameters
-LW = 10; LH = 1; LG = 3;
-m = 1; Ic = (1/12)*(LH^2 + LW^2);
-g = 9.81; k = 20; k_list = [.5*k, .5*k, 2*k, 5*k];
-l0 = 1.5*LG;
-Pbl_box = [-LW; -LH] / 2;
-Pbr_box = [LW; -LH] / 2;
-Ptl_box = [-LW; LH] / 2;
-Ptr_box = [LW; LH] / 2;
-boundary_pts = [Pbl_box, Pbr_box, Ptr_box, Ptl_box, Pbl_box];
-Pbl1_world = Pbl_box + [-LG; -LG];
-Pbl2_world = Pbl_box + [LG; -LG];
-Pbr1_world = Pbr_box + [0; -l0];
-Pbr2_world = Pbr_box + [l0; 0];
-P_world = [Pbl1_world, Pbl2_world, Pbr1_world, Pbr2_world];
-P_box = [Pbl_box, Pbl_box, Pbr_box, Pbr_box];
-% Define system parameters
-box_params = struct();
-box_params.m = m;
-box_params.I = Ic;
-box_params.g = g;
-box_params.k_list = k_list;
-box_params.l0_list = l0 * ones(size(P_world, 2));
-box_params.P_world = P_world;
-box_params.P_box = P_box;
-box_params.boundary_pts = boundary_pts;
+ % define system parameters
+    box_params = struct();
+    box_params.m = 1;
+    box_params.g = 9.81;
+    box_params.k_list = [0.25; 0.25; 0.25; 0.25; 0.25; 0.25; 0.25; 0.25];
+    box_params.l0_list = [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25];
+    box_params.P_world = [-3, -5, -6, -5, 2, 5, 5, 3;
+                          -3, -3, 3, 4, 4, 3, -3, -3];
+    box_params.P_box = [-4, -4, -4, -4, 4, 4, 4, 4;
+                        -2, -2, 2, 2, 2, 2, -2, -2];
+
+    % box width and height
+    box_width = abs(box_params.P_box(1, 3) - box_params.P_box(1, 5));
+    box_height = abs(box_params.P_box(2, 1) - box_params.P_box(2, 3));
+
+    % calculating moment of inertia of box 
+    box_params.I = (1/12)*(box_height^2 + box_width^2);
 
 
 x0 = 0;
